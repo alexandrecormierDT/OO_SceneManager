1341bbb41147ed723de0af6fe7ccc33baf51ce01
alexandre_ooolala <a.cormier@ooolala.fr>
2021-02-08 15:16:57 +0100

    morror to copy to script folder and view class

---
+++ js/OO_SceneManager.js
@@ -0,0 +1,18 @@
+//IMPORT CLASSES : 
+
+include("P:/pipeline/alexdev/OO_SceneManager/js/OO_Class_SceneManager.js");
+include("P:/pipeline/alexdev/OO_SceneManager/js/OO_Class_Asset.js");
+include("P:/pipeline/alexdev/OO_SceneManager/js/OO_Class_AssetManager.js");
+include("P:/pipeline/alexdev/OO_SceneManager/js/OO_Class_Tree.js");
+include("P:/pipeline/alexdev/OO_SceneManager/js/OO_Class_TreeManager.js");
+include("P:/pipeline/alexdev/OO_SceneManager/js/OO_Class_Portal.js");
+include("P:/pipeline/alexdev/OO_SceneManager/js/OO_Class_PortalManager.js");
+include("P:/pipeline/alexdev/OO_SceneManager/js/OO_Class_View.js");
+include("P:/pipeline/alexdev/OO_SceneManager/js/OO_Class_ViewManager.js");
+
+function OO_main(){
+	
+	
+	
+	
+}
