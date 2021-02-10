4927114700c4daa95186d82ce62e4a9e8cbe382c
alexandre_ooolala <a.cormier@ooolala.fr>
2021-02-10 16:28:03 +0100

    proto and master system

---
+++ js/OO_SceneManager_master.js
@@ -0,0 +1,13 @@
+//SCENE MANAGER MIRROR MASTER
+
+var folder = "master"; 
+
+include("openHarmony.js");
+
+include("P:/pipeline/alexdev/"+folder+"/OO_SceneManager_"+folder+"/js/OO_SceneManager.js");
+
+function OO_test(){
+	
+	OO_main();
+	
+}
---
+++ js/OO_SceneManager_proto.js
@@ -0,0 +1,13 @@
+//SCENE MANAGER MIRROR PROTO
+
+var folder = "proto"; 
+
+include("openHarmony.js");
+
+include("P:/pipeline/alexdev/"+folder+"/OO_SceneManager_"+folder+"/js/OO_SceneManager.js");
+
+function OO_test(){
+	
+	OO_main();
+	
+}
