5a67e72c72c8003fdd3c425540f95fad7567b92a
alexandre_ooolala <a.cormier@ooolala.fr>
2021-07-30 17:24:04 +0200

    using environnement variable for script import

--- js/OO_SceneManager_master.js
+++
@@ -1,7 +0,0 @@
-//SCENE MANAGER MIRROR MASTER
-//MessageLog.trace("SCENE MANAGER MIRROR - MASTER");
-
-var FOLDER = "master"; 
-
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/OO_SceneManager.js");
-
--- js/OO_class_import.js
+++ js/OO_class_import.js
@@ -1,129 +1,136 @@
+var ROOTPATH = ""
+if(FOLDER == "proto"){
+    ROOTPATH = System.getenv("SCENEMANAGER_PROTO")
+}else{
+    ROOTPATH = System.getenv("SCENEMANAGER_RELEASE")
+}
+
 
 
 // FOLDER should be declared in previous include : OO_SceneManager_proto or master    enable to switch from folders proto(dev) and master(for users) 
 
 //CLASSES
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_Sanitizer.js");
+include(ROOTPATH+"/js/Classes/OO_Class_Sanitizer.js");
 
 //handy xml and svg parser 
 OO.XML = require("P:/pipeline/extra_soft/pixl-xml-master/modified_xml.js");
 
 
 // IMAGE FILE PARSER CLASS
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/utils/SVG_reader.js");
+include(ROOTPATH+"/js/Classes/utils/SVG_reader.js");
 
 
 
 // SCENE MANAGER 
 
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_SceneManager.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_Log.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_Backdrop.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_BackdropManager.js");
+include(ROOTPATH+"/js/Classes/OO_Class_SceneManager.js");
+include(ROOTPATH+"/js/Classes/OO_Class_Log.js");
+include(ROOTPATH+"/js/Classes/OO_Class_Backdrop.js");
+include(ROOTPATH+"/js/Classes/OO_Class_BackdropManager.js");
 
 // PATHS
 
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_Context.js");
+include(ROOTPATH+"/js/Classes/OO_Class_Context.js");
 
 // RENDER
 
 
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_RenderManager.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_DeadLineJob.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_DeadLineJobSubmiter.js");
+include(ROOTPATH+"/js/Classes/OO_Class_RenderManager.js");
+include(ROOTPATH+"/js/Classes/OO_Class_DeadLineJob.js");
+include(ROOTPATH+"/js/Classes/OO_Class_DeadLineJobSubmiter.js");
 
 //BREAKDOWN 
 
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_ShotgridReader.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_ShotgridLink.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_Breakdown.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_Shot.js");
+include(ROOTPATH+"/js/Classes/OO_Class_ShotgridReader.js");
+include(ROOTPATH+"/js/Classes/OO_Class_ShotgridLink.js");
+include(ROOTPATH+"/js/lasses/OO_Class_Breakdown.js");
+include(ROOTPATH+"/js/Classes/OO_Class_Shot.js");
 
 
 // ASSETS
 
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_Asset.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_AssetManager.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_AssetDetector.js");
+include(ROOTPATH+"/js/Classes/OO_Class_Asset.js");
+include(ROOTPATH+"/js/Classes/OO_Class_AssetManager.js");
+include(ROOTPATH+"/js/Classes/OO_Class_AssetDetector.js");
 
 // POSING
 
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_Posing.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_PosingManager.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_PosingCreator.js");
+include(ROOTPATH+"/js/Classes/OO_Class_Posing.js");
+include(ROOTPATH+"/js/Classes/OO_Class_PosingManager.js");
+include(ROOTPATH+"/js/Classes/OO_Class_PosingCreator.js");
 
 // TREES
 
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_Tree.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_TreeManager.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_PSDReader.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_PSDImporter.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_TPL.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_TPLManager.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_TPLPasseport.js");
+include(ROOTPATH+"/js/Classes/OO_Class_Tree.js");
+include(ROOTPATH+"/js/Classes/OO_Class_TreeManager.js");
+include(ROOTPATH+"/js/Classes/OO_Class_PSDReader.js");
+include(ROOTPATH+"/js/Classes/OO_Class_PSDImporter.js");
+include(ROOTPATH+"/js/Classes/OO_Class_TPL.js");
+include(ROOTPATH+"/js/Classes/OO_Class_TPLManager.js");
+include(ROOTPATH+"/js/Classes/OO_Class_TPLPasseport.js");
 
 // PORTALS
 
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_Portal.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_PortalManager.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_PortalCreator.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_Cadre.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_SVGReader.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_PortalFiter.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_PortalReader.js");
+include(ROOTPATH+"/js/Classes/OO_Class_Portal.js");
+include(ROOTPATH+"/js/Classes/OO_Class_PortalManager.js");
+include(ROOTPATH+"/js/Classes/OO_Class_PortalCreator.js");
+include(ROOTPATH+"/js/Classes/OO_Class_Cadre.js");
+include(ROOTPATH+"/js/Classes/OO_Class_SVGReader.js");
+include(ROOTPATH+"/js/Classes/OO_Class_PortalFiter.js");
+include(ROOTPATH+"/js/Classes/OO_Class_PortalReader.js");
 
 
 
 // SETUP
 
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_SetupManager.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_Setup.js");
+include(ROOTPATH+"/js/Classes/OO_Class_SetupManager.js");
+include(ROOTPATH+"/js/Classes/OO_Class_Setup.js");
 
 
 // SCENE FILES
 
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_FileManager.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_SceneFilesManager.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_Stage.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_ImageFile.js");
+include(ROOTPATH+"/js/Classes/OO_Class_FileManager.js");
+include(ROOTPATH+"/js/Classes/OO_Class_SceneFilesManager.js");
+include(ROOTPATH+"/js/Classes/OO_Class_Stage.js");
+include(ROOTPATH+"/js/Classes/OO_Class_ImageFile.js");
 
 //ELEMENTS
 
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_ElementManager.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_ElementFolder.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_TVG.js");
+include(ROOTPATH+"/js/Classes/OO_Class_ElementManager.js");
+include(ROOTPATH+"/js/Classes/OO_Class_ElementFolder.js");
+include(ROOTPATH+"/js/Classes/OO_Class_TVG.js");
 
 //LIBRARY
 
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_LibraryManager.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_TPL.js");
+include(ROOTPATH+"/js/Classes/OO_Class_LibraryManager.js");
+include(ROOTPATH+"/js/Classes/OO_Class_TPL.js");
 
 // MASTER CONTROLLERS
 
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_ScriptFolder.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_TBstate.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_MasterControler.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_MCManager.js");
+include(ROOTPATH+"/js/Classes/OO_Class_ScriptFolder.js");
+include(ROOTPATH+"/js/Classes/OO_Class_TBstate.js");
+include(ROOTPATH+"/js/Classes/OO_Class_MasterControler.js");
+include(ROOTPATH+"/js/Classes/OO_Class_MCManager.js");
 
 
 //AUTOLISPING 
 
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_StoryboardOutputManager.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_VoiceTrack.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_LipsDetectionManager.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_LipsDetectionGenerator.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_Lipsing.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_Phoneme.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_LipsInjector.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_CharacterDetector.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_HeadAngle.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_LipsImporter.js");
+include(ROOTPATH+"/js/Classes/OO_Class_StoryboardOutputManager.js");
+include(ROOTPATH+"/js/Classes/OO_Class_VoiceTrack.js");
+include(ROOTPATH+"/js/Classes/OO_Class_LipsDetectionManager.js");
+include(ROOTPATH+"/js/Classes/OO_Class_LipsDetectionGenerator.js");
+include(ROOTPATH+"/js/Classes/OO_Class_Lipsing.js");
+include(ROOTPATH+"/js/Classes/OO_Class_Phoneme.js");
+include(ROOTPATH+"/js/Classes/OO_Class_LipsInjector.js");
+include(ROOTPATH+"/js/Classes/OO_Class_CharacterDetector.js");
+include(ROOTPATH+"/js/Classes/OO_Class_HeadAngle.js");
+include(ROOTPATH+"/js/Classes/OO_Class_LipsImporter.js");
 
 
 // SHOTGUN
 
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_ViewManager.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_View.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_SGVersion.js");
-include("P:/pipeline/alexdev/"+FOLDER+"/OO_SceneManager_"+FOLDER+"/js/Classes/OO_Class_SGBridge.js");
+include(ROOTPATH+"/js/Classes/OO_Class_ViewManager.js");
+include(ROOTPATH+"/js/Classes/OO_Class_View.js");
+include(ROOTPATH+"/js/Classes/OO_Class_SGVersion.js");
+include(ROOTPATH+"/js/Classes/OO_Class_SGBridge.js");
 	
